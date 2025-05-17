a906cf92f97e16c9b768e4e66a03f7dfcb8323da
bmatt468 <benjamin@benjaminrmatthews.com>
2025-05-17 01:13:59 -0400

    working theme toggle

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,6 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   // ...eslintPluginAstro.configs['jsx-a11y-recommended'],
   {
