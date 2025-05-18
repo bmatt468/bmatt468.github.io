947cf5d9bbdfc44b0c0d53ea90406031893f8298
bmatt468 <benjamin@benjaminrmatthews.com>
2025-05-18 15:30:36 -0400

    all the icons

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
+// Experience Types
+export interface ExperienceNode {
+  timeframe: string;
+  title: string;
+  company: string;
+  skillIcons: string[];
+}
+
+// Footer Types
 export interface FooterLink {
   description: string;
   icon: string;
