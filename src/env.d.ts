8aab1f4b34cc91ded66dedea4707706b1f2d0212
bmatt468 <benjamin@benjaminrmatthews.com>
2025-05-19 11:20:52 -0400

    all the skill icons

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,13 @@ export interface ExperienceNode {
   timeframe: string;
   title: string;
   company: string;
-  skillIcons: string[];
+  skillIcons: Array<SkillIcon>;
+}
+
+export interface SkillIcon {
+  lightIcon: string;
+  darkIcon: string;
+  tooltip?: string;
 }
 
 // Footer Types
