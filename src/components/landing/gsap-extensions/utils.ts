a7e667b000fd23f96f8e770462e5d26f83e490b8
bmatt468 <benjamin@benjaminrmatthews.com>
2025-05-28 16:55:44 -0400

    gsappppp

--- src/components/landing/gsap-extensions/utils.ts
+++ src/components/landing/gsap-extensions/utils.ts
@@ -1,4 +1,6 @@
 import { gsap } from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export function boostrapGsapExtensions(): void {
   if (
@@ -39,4 +41,40 @@ export function registerBoxTimeline(
       onLeaveBack: () => console.log(`[${gsapBoxId}] Left viewport downward`),
     },
   });
+}
+
+export function smootherSingleton(): ScrollSmoother {
+  const smoother = ScrollSmoother.get();
+
+  if (!smoother) {
+    return ScrollSmoother.create({
+      smooth: 1,
+      effects: true,
+    });
+  }
+
+  return smoother;
+}
+
+export function registerNavigationLinks(): void {
+  const smoother = smootherSingleton();
+
+  document
+    .querySelectorAll<HTMLAnchorElement>('a.progress-link-anchor')
+    .forEach((a) => {
+      a.addEventListener('click', (e) => {
+        if (e) {
+          e.preventDefault();
+        }
+
+        gsap.to(smoother, {
+          scrollTop: Math.min(
+            ScrollTrigger.maxScroll(window),
+            smoother.offset(a.hash, 'top')
+          ),
+          duration: 1.25,
+          ease: 'power1.inOut',
+        });
+      });
+    });
 }
