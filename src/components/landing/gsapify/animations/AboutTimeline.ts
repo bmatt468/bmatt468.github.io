eb5e29bbbdb5c9da4878b53c5df1a3976c52e9fb
bmatt468 <benjamin@benjaminrmatthews.com>
2025-06-18 17:17:31 -0400

    i quit

--- src/components/landing/gsapify/animations/AboutTimeline.ts
+++ src/components/landing/gsapify/animations/AboutTimeline.ts
@@ -5,25 +5,25 @@ import { theme } from '@components/landing/gsapify/Theme.ts';
 export class AboutTimeline extends BoxBoundTimeline {
   constructor() {
     super('aboutTimeline', '#gsapBoxAbout', {
-      markers: true,
+      // markers: true,
       start: 'top top',
-      pin: true,
+      // pin: true,
       end: 'bottom top',
       snap: 'labelsDirectional',
     });
   }
 
   init(): void {
-    const worker = new ScrollTriggerWorker(this, true);
-    const tl = worker.timeline;
-
-    this.timeline
-      .add(tl)
-      .addLabel('start', 0)
-      .addLabel('about', 0.1)
-      .addLabel('passions', 0.35)
-      .addLabel('past', 0.6)
-      .addLabel('projects', 0.85)
-      .addLabel('end', 1);
+    // const worker = new ScrollTriggerWorker(this, true);
+    // const tl = worker.timeline;
+    //
+    // this.timeline
+    //   .add(tl)
+    //   .addLabel('start', 0)
+    //   .addLabel('about', 0.1)
+    //   .addLabel('passions', 0.35)
+    //   .addLabel('past', 0.6)
+    //   .addLabel('projects', 0.85)
+    //   .addLabel('end', 1);
   }
 }
